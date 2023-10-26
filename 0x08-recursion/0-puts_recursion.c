4d148a30e3a452fdabca9dd62afaa4237f1ec393
JamarkJ <jamesmark19094@gmail.com>
2023-10-25 18:40:59 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion
- * 
- * @s: function parameter
- * 
- * Return: Always 0
- */
-
-void _puts_recursion(char *)
-{         
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s)
+{
 	if (*s)
 	{
 	_putchar(*s);
@@ -17,6 +16,4 @@ void _puts_recursion(char *)
 	}
 	else
 	_putchar('\n');
-
-
 }
