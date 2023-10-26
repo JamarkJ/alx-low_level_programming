955a1ecd26a3e1d5c2598464c2a901a5e1046e25
JamarkJ <jamesmark19094@gmail.com>
2023-10-25 18:44:43 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,9 +3,9 @@
 /**
   * _print_rev_recursion - main function
   *
-  * *s: Function parameter
+  * @s: Function parameter
   *
-  * Return: Voidu
+  * Return: Void
   */
 void _print_rev_recursion(char *s)
 {
